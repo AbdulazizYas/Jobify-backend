6f0a84482a83075e7d97b7e3dfadd3cecbf7bf2e
SaudYM <alyamisaud7@gmail.com>
2022-11-25 19:26:49 +0300

    BLNGtoHSMNY #2

--- models/administrator.js
+++ models/administrator.js
@@ -1,23 +1,32 @@
 const administrator = sequelize.define(
-    "administrator",
-    { 
-      admin_id: {
-          type: Sequelize.UUID,
-          primaryKey: true
-      } 
-    },
-    { 
-      firstName: {
-          type: Sequelize.STRING,
-          primaryKey: false
-      } 
-    },
-    { 
-      lastName: {
-          type: Sequelize.STRING,
-          primaryKey: false
-      }  
-    },
-    //{ timestamps: false }
-  );
-  
+  "administrator",
+  {
+    admin_id: {
+      type: Sequelize.UUID,
+      primaryKey: true
+    }
+  },
+  {
+    firstName: {
+      type: Sequelize.STRING,
+      primaryKey: false
+    }
+  },
+  {
+    lastName: {
+      type: Sequelize.STRING,
+      primaryKey: false
+    }
+  },
+  //{ timestamps: false }
+);
+
+administrator.associate = (models) => {
+  administrator.belongTo(models.users, {
+    foreignKey: "userName",
+    as: "userName"
+  });
+
+};
+
+return administrator;
--- models/companies.js
+++ models/companies.js
@@ -1,16 +1,28 @@
 const companies = sequelize.define(
-    "companies",
-    { 
-      companyName: {
-          type: Sequelize.STRING,
-          primaryKey: true
-      }  
-    },
-    { 
-      location: {
-          type: Sequelize.STRING,
-          primaryKey: false
-      }  
-    },
-    //{ timestamps: false }
-  );
+  "companies",
+  {
+    companyName: {
+      type: Sequelize.STRING,
+      primaryKey: true
+    }
+  },
+  {
+    location: {
+      type: Sequelize.STRING,
+      primaryKey: false
+    }
+  },
+  //{ timestamps: false }
+);
+companies.associate = (models) => {
+  companies.belongTo(models.users, {
+    foreignKey: "userName",
+    as: "userName"
+  });
+  companies.hasMany(models.jobs, {
+    foreignKey: "job_id",
+    as: "job_id"
+  });
+};
+
+return companies;
