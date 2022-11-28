0ca9f8109438d0900c187c15ba5254a7df8c1371
xKusabimaru <khalidsa890@gmail.com>
2022-11-28 23:35:54 +0300

    test

---
+++ testing/test.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const x = {
+    userName: "sniperxx",
+    password: "123",
+    email: "d70me@gmail.com",
+    type: "Company",
+    companyName: "xxx",
+    location: "dammam"
+}
+
+const y = JSON.stringify(x)
+
+app.post("/User.controler/create", function (req, res) {
+    res.json(y);
+});
+
+app.get("/User.controler/getAllusers", function (req, res) {
+    // res.json(y);
+});
