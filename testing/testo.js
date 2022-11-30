1a6fdf299b26296d96dd7a7538ae56de58312b73
SaudYM <alyamisaud7@gmail.com>
2022-11-30 21:02:13 +0300

    ignore plz

--- testing/testo.js
+++ testing/testo.js
@@ -21,52 +21,39 @@
 //     runQueries(db);
 // });
 
-var sqlite3 = require('sqlite3');
-let db= new sqlite3.Database('./mcu.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
-    if (err && err.code == "SQLITE_CANTOPEN") {
-        createDatabase();
-        return;
-        } else if (err) {
-            console.log("Getting error " + err);
-            exit(1);
+var sqlite3 = require('sqlite3').verbose();
+let dba= new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE , (err) => {
+    if (err) {
+       console.log(err.message)
     }
-    runQueries(db);
+   
 });
 
-function createDatabase() {
-    var newdb = new sqlite3.Database('mcu.db', (err) => {
-        if (err) {
-            console.log("Getting error " + err);
-            exit(1);
-        }
-        createTables(newdb);
-    });
-}
+// const x = {
+//     userName: "sniperxx",
+//     password: "123",
+//     email: "d70me@gmail.com",
+//     type: "Company",
+//     companyName: "xxx",
+//     location: "dammam"
+// }
 
-const x = {
-    userName: "sniperxx",
-    password: "123",
-    email: "d70me@gmail.com",
-    type: "Company",
-    companyName: "xxx",
-    location: "dammam"
-}
+// const y = JSON.stringify(x)
 
-const y = JSON.stringify(x)
+// app.post("/User.controler/create", function (req, res) {
+//     res.json(y);
+// });
 
-app.post("/User.controler/create", function (req, res) {
-    res.json(y);
-});
+//     db.run(sql, x, function (err, result) {
+//         if (err) {
+//             res.send(Json.stringify(err));
+//         }
+//         else {
+//             res.send(SJSON);
+//         }
+//     });
+//     db.close();
 
-    db.run(sql, x, function (err, result) {
-        if (err) {
-            res.send(Json.stringify(err));
-        }
-        else {
-            res.send(SJSON);
-        }
-    });
-    db.close();
-
-    var jobModel = require("../controllers/Job.controler");
-    console.log(jobModel.getAlljobs);
+//     var jobModel = require("../controllers/Job.controler");
+// const db = require('../models');
+//     console.log(jobModel.getAlljobs);
