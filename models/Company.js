6f261138b80e4f93b91ebe103454f06612f1ee8d
SaudYM <alyamisaud7@gmail.com>
2022-12-13 19:36:31 +0300

    commint3

--- models/Company.js
+++ models/Company.js
@@ -1,48 +1,48 @@
 module.exports = (sequelize, DataTypes) => {
-const Company = sequelize.define(
-  "Company",
-  {
-    registration_id: {
-      type: sequelize.UUID,
-      primaryKey: true,
-      allowNull: false,
-      validate: {
-        notNull: { msg: 'company must have an id' },
-      },
-    }
-  },
-  {
-    companyName: {
-      type: sequelize.STRING,
-      primaryKey: false,
-      allowNull: false,
-      validate: {
-        notNull: { msg: 'company must have a  name' },
-      },
-    }
-  },
-  {
-    location: {
-      type: sequelize.STRING,
-      primaryKey: false,
-      allowNull: false,
-      validate: {
-        notNull: { msg: 'company must have a location' },
-      },
-    }
-  },
-  //{ timestamps: false }
-);
-Company.associate = (models) => {
-  Company.belongTo(models.User, {
-    foreignKey: "userName",
-    as: "userName"
-  });
-  Company.hasMany(models.Job, {
-    foreignKey: "job_id",
-    as: "job_id"
-  });
-};
+  const Company = sequelize.define(
+    "Company",
+    {
+      registration_id: {
+        type: sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'company must have an id' },
+        },
+      }
+    },
+    {
+      companyName: {
+        type: sequelize.STRING,
+        primaryKey: false,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'company must have a  name' },
+        },
+      }
+    },
+    {
+      location: {
+        type: sequelize.STRING,
+        primaryKey: false,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'company must have a location' },
+        },
+      }
+    },
+    //{ timestamps: false }
+  );
+  Company.associate = (models) => {
+    Company.belongTo(models.User, {
+      foreignKey: "userName",
+      as: "userName"
+    });
+    Company.hasMany(models.Job, {
+      foreignKey: "job_id",
+      as: "job_id"
+    });
+  };
 
-return Company;
+  return Company;
 };
